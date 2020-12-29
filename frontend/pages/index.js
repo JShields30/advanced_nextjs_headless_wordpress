bcc418bf602cdc71b6d7bbe3d6182dde4c3be5d4
Joe Shields <shields.john.joseph@gmail.com>
2020-12-28 22:08:21 -0600

    added tw component to index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,6 +3,8 @@ import Head from 'next/head'
 
 export default function Index() {
   return (
-    <div>Hello World</div>
+    <div> <h3 class="text-lg leading-6 font-medium text-gray-900">
+    Applicant Information
+  </h3></div>
   )
 }
